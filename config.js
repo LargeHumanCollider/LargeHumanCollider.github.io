501e690e14b5152a1acd6a6c79d505c205bacfa4
Xavier <xcoadic@gmail.com>
2016-11-03 17:57:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ GH.OWNER = 'multibao'
 GH.CREW = 'organisations'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'Partager et enrichir vos pratiques d\'équipe'
-MULTIBAO.UVP2 = 'Let\'s Do It Together'
+MULTIBAO.UVP1 = 'Concevoir et partager ensemble des petits bouts de trucs'
+MULTIBAO.UVP2 = 'Particules élémentaires'
 MULTIBAO.BUTTON1 = 'Découvrir'
 MULTIBAO.BUTTON2 = 'Espace contributeurs / bloggeurs'
